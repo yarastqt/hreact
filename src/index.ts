156ecb2861b83cb9e371c5e7678d409e1d1910e0
yarastqt <yarastqt@yandex-team.ru>
2019-12-01 21:16:17 +0300

    feat: impl base render

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 // hreact
 
-const render = (element: any, container: any) => {}
+type Maybe<T> = T | null
+
+const render = (element: HReactElement, container: Maybe<HTMLElement>) => {
+  if (container === undefined || container === null) {
+    throw new Error('Container should be is valid DOM element.')
+  }
+  const domElement = document.createElement(element.type)
+  container.appendChild(domElement)
+}
 
 const createTextElement = (text: string) => {
   return {
