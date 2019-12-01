caa952e052557c4bdb2ec76dfbc34feb417870d8
yarastqt <yarastqt@yandex-team.ru>
2019-12-01 21:07:42 +0300

    feat: impl createElement

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
 // hreact
 
 const render = (element: any, container: any) => {}
+
+const createTextElement = (text: string) => {
+  return {
+    type: 'λ_TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+type HReactElement = {
+  type: string
+  props: {
+    children: string[]
+  }
+}
+
+const createElement = (type: string, props: any, children: string[] | string): HReactElement => {
+  const childs = Array.isArray(children) ? children : [children]
+  const processedChilds = childs.map(child => {
+    return typeof child === 'object'
+      ? child
+      : createTextElement(child)
+  })
+
+  return {
+    type,
+    props: {
+      // TODO: Handle children from props and convert them into array.
+      ...props,
+      children: processedChilds,
+    },
+  }
+}
